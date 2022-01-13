ce9c2c7c2bedc6e7aec7e06aab6fba9ccd7ba3fa
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-01-12 17:31:36 -0800

    Create LOGIN_JSON.py

---
+++ JSON-PARSER/LOGIN_JSON.py
@@ -0,0 +1,20 @@
+import json
+
+'''
+  * Takes User Info and converts to str and dict json. 
+'''
+
+login_info= {
+    "USER": "TEST",
+    "PASS": "TEST",
+}
+
+json_string = json.dumps(login_info, indent=3)
+with open('config_str.json', 'w') as f:
+    f.write(json_string)
+    #
+
+json_dict = json.dumps(login_info, indent=3)
+with open('config_dict.json', 'w') as fp:
+    json.dump(login_info, fp)
+
