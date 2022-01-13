82c3c92fbf5b62a310ffadb90068de447494df8d
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-01-12 17:41:46 -0800

    Update LOGIN_JSON.py

--- TOOLS/LOGIN_JSON.py
+++ TOOLS/LOGIN_JSON.py
@@ -4,6 +4,8 @@ import json
   * Takes User Info and converts to str and dict json. 
 '''
 
+
+ 
 login_info= {
     "USER": "TEST",
     "PASS": "TEST",
@@ -17,3 +19,13 @@ with open('config_str.json', 'w') as f:
 json_dict = json.dumps(login_info, indent=3)
 with open('config_dict.json', 'w') as fp:
     json.dump(login_info, fp)
+
+  
+def read_config_ii():
+    #config_loc = os.getcwd() + "/config.json"
+    with open("config_str.json", 'r') as f:
+        config_data = json.loads(f.read())
+    USER, PASS = config_data.get('USER'), config_data.get('PASS')
+    return USER, PASS
+
+read_config_ii()
